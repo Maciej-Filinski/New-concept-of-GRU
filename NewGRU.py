488f3ec65d71e50570f78a60d14c74ee2d0d7555
Maciej Filinski <maciej.filinski@gmail.com>
2021-04-15 13:39:44 +0200

    The prototype of the new GRU

--- NewGRU.py
+++ NewGRU.py
@@ -1,50 +1,27 @@
-import tensorflow as tf
+from tensorflow.keras import backend as K
 from tensorflow.keras import layers
-from abc import ABC
+from tensorflow import keras
+import tensorflow as tf
 
 
-class NewGRU(tf.keras.Model, ABC):
-    def __init__(self, input_shape, state_length, output_length, *args, **kwargs):
-        super(NewGRU, self).__init__(*args, **kwargs)
-        ''' Create node 1 (DNN) '''
-        self.node_1_input_layer = layers.Dense(32, input_shape=input_shape, activation='sigmoid')
-        self.node_1_output_layer = layers.Dense(state_length, activation='sigmoid')
+class NewGRU(keras.layers.Layer):
+    def __init__(self, unit, **kwargs):
+        self.unit = unit
+        self.state_size = [tf.TensorShape(unit)]
+        super(NewGRU, self).__init__(**kwargs)
 
-        ''' Create node 2 (DNN) '''
-        self.node_2_input_layer = layers.Dense(32, input_shape=input_shape, activation='sigmoid')
-        self.node_2_output_layer = layers.Dense(state_length, activation='sigmoid')
+    def build(self, input_shape):
+        input_shape = input_shape[1]
+        self.kernel = self.add_weight(shape=(input_shape + self.unit, self.unit), initializer="uniform",
+                                      name="kernel")
+        self.recurrent_kernel = self.add_weight(shape=(self.unit, self.unit), initializer="uniform",
+                                                name="recurrent_kernel")
 
-        ''' Create node 3 (DNN) '''
-        self.node_3_input_layer = layers.Dense(32, activation='sigmoid')
-        self.node_3_output_layer = layers.Dense(output_length, activation='sigmoid')
-
-        self.recurrent_state = tf.Variable(initial_value=tf.zeros(shape=(1, state_length)),
-                                           trainable=False,
-                                           validate_shape=False,
-                                           dtype=tf.float32)
-
-    def call(self, inputs, training=None, mask=None):
-        if inputs.shape[0] is None:
-            inputs = inputs[0]
-        network_input = tf.keras.layers.Concatenate(axis=-1)([inputs, self.recurrent_state])
-
-        node_1 = self.node_1_input_layer(network_input)
-
-        node_1 = self.node_1_output_layer(node_1)
-
-        node_2 = self.node_2_input_layer(network_input)
-        node_2 = self.node_2_output_layer(node_2)
-
-        #tf.print(self.recurrent_state)
-        #tf.print(self.recurrent_state.shape)
-
-        hat_ht = tf.math.subtract(node_1, self.recurrent_state)
-        f_t = tf.math.multiply(node_2, hat_ht)
-        self.recurrent_state.assign(tf.math.add(f_t, self.recurrent_state))
-
-        #tf.print(self.recurrent_state)
-        #tf.print(self.recurrent_state.shape)
-
-        node_3 = self.node_3_input_layer(self.recurrent_state)
-        node_3 = self.node_3_output_layer(node_3)
-        return node_3
+    def call(self, inputs, states):
+        states = states[0]
+        inputs = tf.concat([inputs, states], -1)
+        output_1 = tf.matmul(inputs, self.kernel)
+        output_2 = tf.matmul(states, self.recurrent_kernel)
+        new_states = states + output_1 + output_2
+        tf.print('output = ', inputs, 'old NN state = ', states, 'new NN state = ', new_states)
+        return output_1, new_states
--- main.py
+++ main.py
@@ -1,48 +1,23 @@
 from NewGRU import NewGRU
+from tensorflow import keras
+from tensorflow.keras.layers import RNN
 import tensorflow as tf
 from SystemDefinition import LinearSystem
 import matplotlib.pyplot as plt
 import numpy as np
 
-def create_model(number_of_system_input, state_length, number_of_system_output):
-    model = NewGRU(input_shape=(1, number_of_system_input),
-                   state_length=state_length,
-                   output_length=number_of_system_output)
-    return model
+state_length = 10
+time_step = 32
+numbers_of_system_input = 1
+batch_size = 2
 
+cell = NewGRU(state_length)
+rnn = keras.layers.RNN(cell)
 
-def train_model(inputs, outputs, neural_network_model: NewGRU):
-    opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
-    neural_network_model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(inputs, outputs, epochs=10)
+input_1 = keras.Input((time_step, numbers_of_system_input))
+outputs = rnn(input_1)
+model = keras.models.Model(input_1, outputs)
 
-
-if __name__ == "__main__":
-    number_of_system_input = 10
-    state_length = 3
-    number_of_system_output = 10
-    model = create_model(number_of_system_input, state_length, number_of_system_output)
-    x = tf.random.uniform(shape=(1, number_of_system_input))
-    print(model(x))
-    print(model(x))
-
-    train_inputs = tf.random.uniform(shape=(1000, 1, number_of_system_input))
-    train_outputs = tf.multiply(train_inputs, train_inputs)
-    train_model(train_inputs, train_outputs, model)
-    test_input = tf.random.uniform(shape=(10000, 1, number_of_system_input))
-    test_output = tf.multiply(test_input, test_input)
-    predict_output = np.zeros(test_input.shape)
-    print(test_input[1, :, :])
-    model(test_input[1, :, :])
-    for i in range(10000):
-        predict_output[i, :, :] = model(test_input[i, :, :])
-        #print(predict_output[i, :, :])
-    print(f' test input = {test_input.shape}')
-    print(f' predict = {predict_output.shape}')
-    print(f' test input = {test_output.shape}')
-
-    plt.plot(test_output[:, :, 0], label='real system')
-    plt.plot(predict_output[:, :, 0], label='predict output')
-    plt.legend()
-    plt.show()
-
+model.compile(optimizer="adam", loss="mse", metrics=["accuracy"])
+model.summary()
+print(model(tf.random.uniform(shape=(batch_size, time_step, numbers_of_system_input))))
