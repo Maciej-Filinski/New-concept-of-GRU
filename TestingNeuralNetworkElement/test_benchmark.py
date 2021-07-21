8d75bc5578d068b7bed42d3274f911744853c34f
Koen Tiels <45268989+koeti602@users.noreply.github.com>
2021-07-21 14:46:24 +0200

    Update test_benchmark.py
    Load Silverbox data.

--- TestingNeuralNetworkElement/test_benchmark.py
+++ TestingNeuralNetworkElement/test_benchmark.py
@@ -4,6 +4,12 @@ import matplotlib
 import matplotlib.pyplot as plt
 from data_generator import *
 from build_model import *
+from pathlib import Path
+import scipy.io
+import os
+import urllib
+import urllib.request
+import zipfile
 
 """
 Simulation parameters
@@ -11,20 +17,80 @@ WARNING!
 TRAIN_DATA_LENGTH must be a multiple of the BATCH_SIZE and higher or equal TEST_DATA_LENGTH
 """
 EPOCHS = 2
-BATCH_SIZE = 50
-TRAIN_DATA_LENGTH = 1000
-TEST_DATA_LENGTH = 100
+BATCH_SIZE = 40
+TRAIN_DATA_LENGTH = 81920
+TEST_DATA_LENGTH = 40400
 NUMBER_OF_INPUTS = 1
 NUMBER_OF_OUTPUTS = 1
 
 
 def benchmark_data():
-    x_train_benchmark = np.zeros(TRAIN_DATA_LENGTH, NUMBER_OF_INPUTS)
-    y_train_benchmark = np.zeros(TRAIN_DATA_LENGTH, NUMBER_OF_OUTPUTS)
-    x_test_benchmark = np.zeros(TEST_DATA_LENGTH, NUMBER_OF_INPUTS)
-    y_test_benchmark = np.zeros(TEST_DATA_LENGTH, NUMBER_OF_OUTPUTS)
+    """
+    Load Silverbox data.
+    Note: Based on https://github.com/antonior92/sysid-neuralnet/blob/master/python-scripts/data/silverbox.py
+    """
+    # Extract input and output data Silverbox
+    mat = scipy.io.loadmat(maybe_download_and_extract())
+    u = mat['V1'][0]  # Input
+    y = mat['V2'][0]  # Output
+
+    # Number of samples of each subset of data
+    n_zeros = 100  # Number of zeros at the start
+    n_test = 40400  # Number of samples in the test set
+    n_trans_before = 460  # Number of transient samples before each multisine realization
+    n = 8192  # Number of samples per multisine realization
+    n_trans_after = 40  # Number of transient samples after each multisine realization
+    n_block = n_trans_before + n + n_trans_after
+    n_multisine = 10  # Number of multisine realizations
+    
+    # Extract training data
+    u_train = np.zeros(n_multisine * n)
+    y_train = np.zeros(n_multisine * n)
+    for i, r in enumerate(n_multisine):
+        u_train[i * n + np.arange(n)] = u[n_zeros + n_test + r * n_block + n_trans_before + np.arange(n)]
+        y_train[i * n + np.arange(n)] = y[n_zeros + n_test + r * n_block + n_trans_before + np.arange(n)]
+    
+    # Extract test data
+    u_test = u[n_zeros:n_zeros + n_test]
+    y_test = y[n_zeros:n_zeros + n_test]
+
+    # Reshape to correct dimensions
+    x_train_benchmark = np.reshape(u_train, newshape=(TRAIN_DATA_LENGTH, NUMBER_OF_INPUTS))
+    y_train_benchmark = np.reshape(y_train, newshape=(TRAIN_DATA_LENGTH, NUMBER_OF_OUTPUTS))
+    x_test_benchmark = np.reshape(u_test, newshape=(TEST_DATA_LENGTH, NUMBER_OF_INPUTS))
+    y_test_benchmark = np.reshape(y_test, newshape=(TEST_DATA_LENGTH, NUMBER_OF_OUTPUTS))
 
     return x_train_benchmark, y_train_benchmark, x_test_benchmark, y_test_benchmark
+
+def maybe_download_and_extract():
+    """
+    Download the data from nonlinear benchmark website, unless it's already here.
+    Note: Taken from https://github.com/antonior92/sysid-neuralnet/blob/master/python-scripts/data/silverbox.py
+    """
+    src_url = 'http://www.nonlinearbenchmark.org/FILES/BENCHMARKS/SILVERBOX/SilverboxFiles.zip'
+    home = Path.home()
+    work_dir = str(home.joinpath('datasets/SilverBox'))
+    if not os.path.exists(work_dir):
+        os.makedirs(work_dir)
+    zipfilepath = os.path.join(work_dir, "SilverboxFiles.zip")
+    if not os.path.exists(zipfilepath):
+        filepath, _ = urllib.request.urlretrieve(
+            src_url, zipfilepath)
+        file = os.stat(filepath)
+        size = file.st_size
+        print('Successfully downloaded', 'SilverboxFiles.zip', size, 'bytes.')
+    else:
+        print('SilverboxFiles.zip', 'already downloaded!')
+
+    datafilepath = os.path.join(work_dir, "SilverboxFiles/SNLS80mV.mat")
+    print(datafilepath)
+    if not os.path.exists(datafilepath):
+        zip_ref = zipfile.ZipFile(zipfilepath, 'r')
+        zip_ref.extractall(work_dir)
+        zip_ref.close()
+        print('Successfully unzipped data')
+    return datafilepath
+
 """
 Learning model
 """
